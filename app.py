12f0bc55989a08723e63ea19ec99ddfebccb4ea1
mwadedy <mohamadnor2001@gmail.com>
2020-09-10 13:16:14 +0200

    ping-pong-game
    this file for ping-pong game

---
+++ app.py
@@ -0,0 +1,128 @@
+# import turtle module
+import turtle
+wind = turtle.Screen()  # intialize sceern
+wind.title("Ping Pong By WADEDY")  # set the title of the window
+wind.bgcolor("black")  # set the background color of the window
+wind.setup(width=800, height=600)  # set the width and height of the window
+wind.tracer(0)  # stops the window form updating automatically
+
+
+# madrab1
+madrab1 = turtle.Turtle()  # intializes turtle object (shape)
+madrab1.speed(0)  # set the speed of the animation
+madrab1.shape("square")  # set the shape of the object
+madrab1.color("green")  # set the color of the shape
+# stretch the shapte to meet the size
+madrab1.shapesize(stretch_wid=5, stretch_len=1)
+madrab1.penup()  # stop the object from drawing lines
+madrab1.goto(-350, 0)  # set the position of the object
+
+# madrab2
+madrab2 = turtle.Turtle()
+madrab2.speed(0)
+madrab2.shape("square")
+madrab2.color("red")
+madrab2.shapesize(stretch_wid=5, stretch_len=1)
+madrab2.penup()
+madrab2.goto(350, 0)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("yellow")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.5  # set the ball speed
+ball.dy = 0.5
+
+# score
+score1 = 0
+score2 = 0
+score = turtle.Turtle()
+score.speed(0)
+score.color("orange")
+score.penup()
+score.hideturtle()
+score.goto(0, 260)
+score.write("Player I: 0 Player II: 0", align="center",
+            font=("Courier", 24, "normal"))
+
+
+# functions
+
+
+def madrab1_up():
+    y = madrab1.ycor()  # get the y coordinate of the madrab1
+    y += 20  # set the y to decrease by 20
+    madrab1.sety(y)  # set the y of the madrab1 to the new y coordinate
+
+
+def madrab1_down():
+    y = madrab1.ycor()
+    y -= 20
+    madrab1.sety(y)
+
+
+def madrab2_up():
+    y = madrab2.ycor()
+    y += 20
+    madrab2.sety(y)
+
+
+def madrab2_down():
+    y = madrab2.ycor()
+    y -= 20
+    madrab2.sety(y)
+
+
+# keyboard bindings
+wind.listen()  # tell the window to expect keyborad input
+# when pressing w the function madrab1_up is invoked
+wind.onkeypress(madrab1_up, "w")
+wind.onkeypress(madrab1_down, "s")
+wind.onkeypress(madrab2_up, "Up")
+wind.onkeypress(madrab2_down, "Down")
+
+# main game loop
+while True:
+    wind.update()  # updates the screen everytime the loop run
+
+    # move the ball
+    # ball starts at 0 and everytime loops run--->+0.5 xaxis
+    ball.setx(ball.xcor() + ball.dx)
+    # ball starts at 0 and everytime loops run--->+0.5 yaxis
+    ball.sety(ball.ycor() + ball.dy)
+
+    # border check , top border +300px, bottom border -300px, ball is 20px
+    if ball.ycor() > 290:  # if ball is at top border
+        ball.sety(290)  # set y coordinate +290
+        ball.dy *= -1  # revese direction, making +0.5--->-0.5
+
+    if ball.ycor() < -290:  # if ball is at bottom border
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:  # if ball is at right border
+        ball.goto(0, 0)  # return ball to center
+        ball.dx *= -1  # reverse the x direction
+        score1 += 1
+        score.clear()
+        score.write("Player I: {} Player II: {}".format(score1, score2), align="center",
+                    font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:  # if ball is at left border
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score2 += 1
+        score.clear()
+        score.write("Player I: {} Player II: {}".format(score1, score2), align="center",
+                    font=("Courier", 24, "normal"))
+    # tasadom madrab and ball
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < madrab2.ycor() + 40 and ball.ycor() > madrab2.ycor()-40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < madrab1.ycor() + 40 and ball.ycor() > madrab1.ycor()-40):
+        ball.setx(-340)
+        ball.dx *= -1
